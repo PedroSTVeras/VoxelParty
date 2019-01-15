397920b0bc968e7a053a6223da0ee39e2abeacf9
Pedro Veras <pedroveras12@gmail.com>
2019-01-15 01:19:41 -0200

    ssss

--- Project/Scripts/MainMenu.gd
+++ Project/Scripts/MainMenu.gd
@@ -1,19 +1,49 @@
 extends Node2D
 
-onready var playButton = $Play
-onready var miniGamesButton = $MiniGames
-onready var profilesButton = $Profiles
-onready var optionsButton = $Options
-onready var exitButton = $Exit
+#Buttons
+onready var playButton = $Buttons/Play
+onready var miniGamesButton = $Buttons/MiniGames
+onready var profilesButton = $Buttons/Profiles
+onready var optionsButton = $Buttons/Options
+onready var exitButton = $Buttons/Exit
 
+#Arrow
+onready var arrow = $Arrow
+
+#Selected Button
+var buttonNum = 0
 
 func _ready():
 	playButton.connect("pressed", self, "PlayGame")
 	exitButton.connect("pressed",self, "ExitGame")
 	pass
 
+func _process(delta):
+	
+	#Control arrow
+	if Input.is_action_just_pressed("ui_leftJoy_up") || Input.is_action_just_pressed("ui_up"):
+		buttonNum -= 1
+		if buttonNum <= -1:
+			buttonNum = 4
+	if Input.is_action_just_pressed("ui_leftJoy_down") || Input.is_action_just_pressed("ui_down"):
+		buttonNum += 1
+		if buttonNum >= 5:
+			buttonNum = 0
+	
+	#Move arrow
+	arrow.rect_position.y = 210 + buttonNum * 60;
+	
+	#Activate selected button
+	if Input.is_action_just_pressed("ui_A"):
+		if buttonNum == 0:
+			PlayGame()
+		elif buttonNum == 4:
+			ExitGame()
+	
+	pass
+
 func PlayGame():
-	get_tree().change_scene("res://Scenes/Map/TestMap.tscn");
+	get_tree().change_scene("res://Scenes/Menus/PlayerSelectMenu.tscn");
 	pass
 
 func ExitGame():
