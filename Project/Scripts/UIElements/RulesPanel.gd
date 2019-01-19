a2384635d434de35802d3c5a2b7b61e7258fa690
Pedro Veras <pedroveras12@gmail.com>
2019-01-19 21:32:17 -0200

    Rules Menu

--- Project/Scripts/UIElements/RulesPanel.gd
+++ Project/Scripts/UIElements/RulesPanel.gd
@@ -4,7 +4,6 @@ onready var selectPanel = $Panel
 onready var selectPanel2 = $NextPanel
 onready var playButton = $PlayButton
 
-var column = 0
 var row = 0
 
 func _ready():
@@ -14,27 +13,18 @@ func _ready():
 func _process(delta):
 	
 	#Control panel
-	if Input.is_action_just_pressed("ui_leftJoy_right") || Input.is_action_just_pressed("ui_right"):
-		column += 1
-		if column >= 4:
-			column = 0
-	if Input.is_action_just_pressed("ui_leftJoy_left") || Input.is_action_just_pressed("ui_left"):
-		column -= 1
-		if column <= -1:
-			column = 3
 	if Input.is_action_just_pressed("ui_leftJoy_up") || Input.is_action_just_pressed("ui_up"):
 		row -= 1
 		if row <= -1:
-			row = 2
+			row = 5
 	if Input.is_action_just_pressed("ui_leftJoy_down") || Input.is_action_just_pressed("ui_down"):
 		row += 1
-		if row >= 3:
+		if row >= 6:
 			row = 0
 	
 	#Move arrow
-	selectPanel.rect_position.x = 10 + column * 200;
-	selectPanel.rect_position.y = 50 + row * 230;
-	if row == 2:
+	selectPanel.rect_position.y = 70 + row * 80;
+	if row == 5:
 		selectPanel.hide()
 		selectPanel2.show()
 	else:
@@ -43,13 +33,9 @@ func _process(delta):
 	
 	#Activate selected button
 	if Input.is_action_just_pressed("ui_A"):
-		#Change character
-		if (row !=2):
-			get_node("PlayerButtons/PlayerButton"+str(1+column+(row*4))).ChangeType()
-		else:
-			NextScreen()
+		NextScreen()
 	
 	pass
 
 func NextScreen():
-	get_tree().change_scene("res://Scenes/Menus/RulesSelectMenu.tscn");
+	get_tree().change_scene("res://Scenes/Map/TestMap.tscn");
